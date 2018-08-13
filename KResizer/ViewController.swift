2632120014250fddf6a603ac039882522b18fae9
Ken Lâm <hoangvu23108@gmail.com>
2018-08-13 16:57:47 +0700

    Initial Commit

---
+++ KResizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KResizer
+//
+//  Created by Ken on 8/13/18.
+//  Copyright © 2018 KPU. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
